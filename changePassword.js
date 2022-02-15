d0aedd01f6c164a8bdf294732ddd7717d5bf0f77
ARAVIND-S-1997 <itsmearavind4@gmail.com>
2022-02-15 15:26:52 +0530

    correction corrected in change password

--- changePassword.js
+++ changePassword.js
@@ -2,6 +2,7 @@ import { client } from "./index.js";
 import express from "express";
 import { genPassword } from "./passwordGenerator.js";
 const router = express.Router();
+import { emailsender } from "./mailtransporter.js"
 
 // Change password Part
 
@@ -9,6 +10,7 @@ export const changepasswordRouter = router;
 
 router.route("/changepassword").post(async (request, response) => {
     const { password, token } = request.body;
+    console.log(password,token)
     const verification = await client.db("login").collection("usersDetails").findOne({ password: { $eq: token } })
     const { emailId } = await verification;
     console.log(emailId)
