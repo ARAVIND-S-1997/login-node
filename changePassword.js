d9473a7833b3f9b51d612d931399c27314bca667
ARAVIND-S-1997 <itsmearavind4@gmail.com>
2022-02-15 15:33:19 +0530

    final

--- changePassword.js
+++ changePassword.js
@@ -17,9 +17,11 @@ router.route("/changepassword").post(async (request, response) => {
     if (verification) {
         const finalPassword = await genPassword(password)
         const updatePassword = await client.db("login").collection("usersDetails").updateOne({ emailId }, { $set: { password: finalPassword } })
+    if(updatePassword){
         response.redirect(`https://epic-mcclintock-d7afca.netlify.app/`)
         const message = (`<p>Password changed sucessFully😊</p>`);
         emailsender(emailId, message, response);
+    }
     }
     else {
         response.send({ message: "link expired" })
